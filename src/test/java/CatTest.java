f320158dc0eb41920693cebbee6caa7b2a9850b0
Mikhail Berezin <iona@ej3.ru>
2024-04-26 22:04:38 +0300

    cat test

---
+++ src/test/java/CatTest.java
@@ -0,0 +1,34 @@
+import com.example.Cat;
+import com.example.Feline;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(MockitoJUnitRunner.class)
+public class CatTest {
+
+    @Mock
+    Feline feline;
+
+    @Test
+    public void getSoundTest() {
+        Cat cat = new Cat(feline);
+        String sound = cat.getSound();
+        assertEquals(sound, "Мяу");
+    }
+
+    @Test
+    public void getFoodTest() throws Exception {
+        Cat cat = new Cat(feline);
+        Mockito.when(feline.eatMeat()).thenReturn(List.of("Животные", "Птицы", "Рыба"));
+        List<String> food = cat.getFood();
+        System.out.println(food);
+        assertEquals(food, List.of("Животные", "Птицы", "Рыба"));
+    }
+}
